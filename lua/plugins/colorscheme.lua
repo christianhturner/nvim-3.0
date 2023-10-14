813bf78814f214183b326b042d826be5aab5992e
Christian Turner <ch.turner94@gmail.com>
2023-10-14 16:45:48 -0400

    init

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "dracula_pro",
+    lazy = "false",
+    dir = "~/.config/dracula_pro/",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula_pro",
+    },
+  },
+}
