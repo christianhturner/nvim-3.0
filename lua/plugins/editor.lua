b10ea80f9f5aed63c7ea54d1b7811c038e9ff836
Christian Turner <cturnrq@amazon.com>
2024-04-02 20:56:38 -0400

    Added APM

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -17,4 +17,16 @@ return {
             },
         },
     },
+    -- VIM APM
+    {
+        "ThePrimeagen/vim-apm",
+        opts = {},
+        config = function(_, opts)
+            local apm = require("vim-apm")
+            apm:setup({})
+            vim.keymap.set("n", "<leader>apm", function()
+                apm:toggle_monitor()
+            end)
+        end,
+    },
 }
