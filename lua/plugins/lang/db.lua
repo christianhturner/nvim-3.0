9b1f05688c81c4eb5188c1917c346237f38867f5
Christian Turner <cturnrq@amazon.com>
2024-04-02 20:54:29 -0400

    (feat) added DB view support
    Added the ability to view DB files, there is still an error that is
    thrown that needs to be resolved for this to work perfectly.

--- lua/plugins/lang/db.lua
+++ lua/plugins/lang/db.lua
@@ -1,12 +1,27 @@
+vim.g.dbee_state = 0
+
 return {
     "kndndrj/nvim-dbee",
     dependencies = {
         "MunifTanjim/nui.nvim",
     },
     build = function()
-        require("dbee").install()
+        require("dbee").install("go")
     end,
     config = function()
         require("dbee").setup()
     end,
+    keys = {
+        {
+            "<C-d>",
+            function()
+                if vim.g.dbee_state == 0 then
+                    require("dbee").open()
+                    vim.g.dbee_state = 1
+                else
+                    require("dbee").toggle()
+                end
+            end,
+        },
+    },
 }
