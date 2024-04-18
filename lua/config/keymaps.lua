edef7dc36f7ad85a5f1e39c126561ecfee9d536b
Christian Turner <cturnrq@amazon.com>
2024-04-17 21:31:56 -0400

    added which-key label for goto Hover doc

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,11 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = vim.keymap.set
+-- local del = vim.keymap.del
 local wk = require("which-key")
 
 wk.register({
     g = {
         x = { "Open Link" },
+    },
+    K = {
+        name = "Hover Doc",
+        K = { "GoTo Hover Doc" },
     },
 })
 
@@ -15,3 +20,5 @@ map("n", "<C-=>", "<C-w>+", { desc = "Increase Window Height" })
 map("n", "<C-->", "<C-w>-", { desc = "Decrease window height" })
 map("n", "<C-,>", "<C-w><", { desc = "Decrease window width" })
 map("n", "<C-.>", "<C-w>>", { desc = "Increase window width" })
+
+-- Disable default Lsp Commands for Lazy, replaced by lspsaga
