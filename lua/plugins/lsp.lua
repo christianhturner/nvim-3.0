9dcf6f30faa11d7de136f67325738e6a432c2e0c
Christian Turner <cturnrq@amazon.com>
2024-04-17 21:32:57 -0400

    fixed lazyvim lsp overriding lspsaga

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,9 +1,23 @@
 return {
     {
+        "nvim-treesitter/nvim-treesitter",
+        ensure_installed = {
+            "smithy",
+        },
+    },
+    {
         "neovim/nvim-lspconfig",
+        -- Disable LSP Keymaps replaced by lspSaga
+        init = function()
+            local keys = require("lazyvim.plugins.lsp.keymaps").get()
+            keys[#keys + 1] = { "K", false }
+        end,
         opts = {
             diagnostics = {
                 inlay_hints = {
+                    enabled = true,
+                },
+                codelens = {
                     enabled = true,
                 },
             },
@@ -27,35 +41,35 @@ return {
                 desc = "Hover doc",
             },
             {
-                "<leader>ca",
-                "<cmd>Lspsaga code_action<cr>",
-                desc = "Code action",
+                "<leader>ck",
+                "<cmd>Lspsaga hover_doc ++keep<cr>",
+                desc = "Pin Hover Doc",
             },
-            {
-                "<leader>cd",
-                "<cmd>Lspsaga show_buf_diagnostics<cr>",
-                desc = "Show line diagnostics",
-            },
-            {
-                "]d",
-                "<cmd>Lspsaga diagnostic_jump_next",
-                desc = "Next diagnostic",
-            },
-            {
-                "[d",
-                "<cmd>Lspsaga diagnostic_jump_prev",
-                desc = "Previous diagnostic",
-            },
-            {
-                "<leader>cD",
-                "<cmd>Lspsaga peek_definition<cr>",
-                desc = "Peak definition",
-            },
-            {
-                "<leader>cT",
-                "<cmd>Lspsaga peek_type_definition<cr>",
-                desc = "Peak type definition",
-            },
+            -- {
+            --     "<leader>cd",
+            --     "<cmd>Lspsaga show_buf_diagnostics<cr>",
+            --     desc = "Show line diagnostics",
+            -- },
+            -- {
+            --     "]d",
+            --     "<cmd>Lspsaga diagnostic_jump_next",
+            --     desc = "Next diagnostic",
+            -- },
+            -- {
+            --     "[d",
+            --     "<cmd>Lspsaga diagnostic_jump_prev",
+            --     desc = "Previous diagnostic",
+            -- },
+            -- {
+            --     "<leader>cD",
+            --     "<cmd>Lspsaga peek_definition<cr>",
+            --     desc = "Peak definition",
+            -- },
+            -- {
+            --     "<leader>cT",
+            --     "<cmd>Lspsaga peek_type_definition<cr>",
+            --     desc = "Peak type definition",
+            -- },
             {
                 "<leader>ci",
                 "<cmd>Lspsaga incoming_calls<cr>",
@@ -66,26 +80,26 @@ return {
                 "<cmd>Lspsaga outgoing_calls<cr>",
                 desc = "Outgoing calls",
             },
-            {
-                "<leader>cs",
-                "<cmd>Lspsaga outline<cr>",
-                desc = "Symbol outline",
-            },
-            {
-                "<leader>cr",
-                "<cmd>Lspsaga rename<cr>",
-                desc = "Rename",
-            },
-            {
-                "<leader>gd",
-                "<cmd>Lspsaga goto_definition<cr>",
-                desc = "Go to definition",
-            },
-            {
-                "<leader>gy",
-                "<cmd>Lspsaga goto_type_definitions<cr>",
-                desc = "Go to type definition",
-            },
+            -- {
+            --     "<leader>cs",
+            --     "<cmd>Lspsaga outline<cr>",
+            --     desc = "Symbol outline",
+            -- },
+            -- {
+            --     "<leader>cr",
+            --     "<cmd>Lspsaga rename<cr>",
+            --     desc = "Rename",
+            -- },
+            -- {
+            --     "<leader>gd",
+            --     "<cmd>Lspsaga goto_definition<cr>",
+            --     desc = "Go to definition",
+            -- },
+            -- {
+            --     "<leader>gy",
+            --     "<cmd>Lspsaga goto_type_definitions<cr>",
+            --     desc = "Go to type definition",
+            -- },
         },
     },
 }
