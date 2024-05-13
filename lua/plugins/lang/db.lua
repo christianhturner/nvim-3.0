97f3df334efaa9de2158a15d35465eda055fa86e
Christian Turner <cturnrq@amazon.com>
2024-05-13 07:24:15 -0400

    +feat improved dbee UI enter and toggle

--- lua/plugins/lang/db.lua
+++ lua/plugins/lang/db.lua
@@ -1,5 +1,17 @@
 vim.g.dbee_state = 0
 
+local function dbeeUIManager()
+    -- if dbee has never been open use open and set state to 1
+    if vim.g.dbee_state == 0 then
+        require("edgy").toggle()
+        require("dbee").open()
+        vim.g.dbee_state = 1
+    else
+        require("edgy").toggle()
+        require("dbee").toggle()
+    end
+end
+
 return {
     "kndndrj/nvim-dbee",
     dependencies = {
@@ -16,9 +28,11 @@ return {
             "<C-d>",
             function()
                 if vim.g.dbee_state == 0 then
+                    require("edgy").toggle()
                     require("dbee").open()
                     vim.g.dbee_state = 1
                 else
+                    require("edgy").toggle()
                     require("dbee").toggle()
                 end
             end,
