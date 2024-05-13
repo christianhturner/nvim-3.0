4dda40b91b97ab766438bed460e584d27d64feb9
Christian Turner <cturnrq@amazon.com>
2024-05-13 07:23:50 -0400

    removed several commits back through comments

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,13 +1,4 @@
 return {
-    -- Auto-Save
-    --[[
-    {
-        "Pocco81/auto-save.nvim",
-        config = function()
-            require("auto-save").setup()
-        end,
-    },
-    ]]
     -- Whichkey Top Level Menus
     {
         "folke/which-key.nvim",
