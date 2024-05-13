ba0e27169f7dd6b0396dd22df0b6ee3bada45dfc
Christian Turner <cturnrq@amazon.com>
2024-05-13 07:23:40 -0400

    +feat utility for auto adding .vscode task

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,11 +1,70 @@
+local function registerLaunchJson()
+    vim.api.nvim_create_autocmd("VimEnter", {
+        once = true,
+        callback = function()
+            local rootDir = require("lazyvim.util.root").get()
+            local vscodeLaunch = ".vscode/launch.json"
+            if vim.uv.fs_stat(rootDir .. "/" .. vscodeLaunch) then
+                require("dap.ext.vscode").load_launchjs()
+                return vim.notify(
+                    "Loaded DAP configs from `.vscode/launch.json`."
+                )
+            end
+            return vim.notify("No `.vscode/launch.json` file to source.")
+        end,
+    })
+end
+
 return {
+    -- Task Runner | Overseer.nvim
+    {
+        "stevearc/overseer.nvim",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            {
+                "akinsho/toggleterm.nvim",
+                config = function()
+                    require("toggleterm").setup({})
+                end,
+            },
+        },
+        opts = {
+            strategy = {
+                "toggleterm",
+                use_shell = true,
+            },
+        },
+        config = function(_, opts)
+            require("overseer").setup(opts)
+            require("overseer").patch_dap(true)
+            require("dap.ext.vscode").json_decode =
+                require("overseer.json").decode
+
+            registerLaunchJson()
+        end,
+    },
     {
         "mistricky/codesnap.nvim",
         build = "make",
+        keys = {
+            {
+                "<leader>cs",
+                -- "<cmd>lua require('codesnap').copy_into_clipboard()<cr>",
+                "<cmd>CodeSnap<cr>",
+                mode = "x",
+                desc = "Copy selected code to your clipboard.",
+            },
+            {
+                "<leader>cS",
+                "<cmd>CodeSnapSave<cr>",
+                mode = "x",
+                desc = "Save code screenshot to directory.",
+            },
+        },
         opts = {
             has_breadcrumbs = true,
             breadcrumbs_separator = " ==> ",
-            watermark = "github.com/chturner94",
+            watermark = "",
         },
         config = function(_, opts)
             local xdg_config = vim.env.XDG_CONFIG_HOME or vim.env.HOME
