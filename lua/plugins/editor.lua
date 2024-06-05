b92840c13feb82189705081c7bf83a17c5ef10d3
Christian Turner <cturnrq@amazon.com>
2024-06-05 09:04:17 -0400

    removed icon picker and apm

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,32 @@
 return {
+    -- {
+    --     -- Icon Picker
+    --     "ziontee113/icon-picker.nvim",
+    --     config = function()
+    --         require("icon-picker").setup({ disable_legacy_commands = true })
+    --     end,
+    --     keys = {
+    --         {
+    --             "<leader>i",
+    --             "<cmd>IconPickerNormal<cr>",
+    --             noremap = true,
+    --             silent = true,
+    --         },
+    --         {
+    --             "<leader>iy",
+    --             "<cmd>IconPickerYank<cr>",
+    --             noremap = true,
+    --             silent = true,
+    --         },
+    --         {
+    --             "<C-i>",
+    --             "<cmd>IconPickerInsert<cr>",
+    --             mode = "i",
+    --             noremap = true,
+    --             silent = true,
+    --         },
+    --     },
+    -- },
     -- Whichkey Top Level Menus
     {
         "folke/which-key.nvim",
@@ -8,18 +36,18 @@ return {
             },
         },
     },
-    -- VIM APM
-    {
-        "ThePrimeagen/vim-apm",
-        opts = {},
-        config = function(_, opts)
-            local apm = require("vim-apm")
-            apm:setup({})
-            vim.keymap.set("n", "<leader>apm", function()
-                apm:toggle_monitor()
-            end)
-        end,
-    },
+    -- VIM APM Haven't found much use, will revist at a later date
+    -- {
+    --     "ThePrimeagen/vim-apm",
+    --     opts = {},
+    --     config = function(_, opts)
+    --         local apm = require("vim-apm")
+    --         apm:setup({})
+    --         vim.keymap.set("n", "<leader>apm", function()
+    --             apm:toggle_monitor()
+    --         end)
+    --     end,
+    -- },
     -- OCTO - Github Plugin
     {
         "pwntester/octo.nvim",
