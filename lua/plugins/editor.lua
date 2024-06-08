e9fb098014068c696901f74c5793bce06d64745b
Christian Turner <cturnrq@amazon.com>
2024-06-08 07:27:31 -0400

    removed custom implementation of dbee and octo and repladed with default neovim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -48,16 +48,4 @@ return {
     --         end)
     --     end,
     -- },
-    -- OCTO - Github Plugin
-    {
-        "pwntester/octo.nvim",
-        requires = {
-            "nvim-lua/plenary.nvim",
-            "nvim-telescope/telescope.nvim",
-            "nvim-tree/nvim-web-devicons",
-        },
-        config = function()
-            require("octo").setup()
-        end,
-    },
 }
