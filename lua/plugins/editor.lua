0e37fac8fa3320ce073d584576cf858eb3953887
Christian Turner <cturnrq@amazon.com>
2025-02-07 17:14:00 -0500

    Set whichkey to older layout modern

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -43,53 +43,10 @@ return {
             })
         end,
     },
-    -- {
-    --     -- Icon Picker
-    --     "ziontee113/icon-picker.nvim",
-    --     config = function()
-    --         require("icon-picker").setup({ disable_legacy_commands = true })
-    --     end,
-    --     keys = {
-    --         {
-    --             "<leader>i",
-    --             "<cmd>IconPickerNormal<cr>",
-    --             noremap = true,
-    --             silent = true,
-    --         },
-    --         {
-    --             "<leader>iy",
-    --             "<cmd>IconPickerYank<cr>",
-    --             noremap = true,
-    --             silent = true,
-    --         },
-    --         {
-    --             "<C-i>",
-    --             "<cmd>IconPickerInsert<cr>",
-    --             mode = "i",
-    --             noremap = true,
-    --             silent = true,
-    --         },
-    --     },
-    -- },
-    -- Whichkey Top Level Menus
-    -- {
-    --     "folke/which-key.nvim",
-    --     opts = {
-    --         defaults = {
-    --             ["<leader>r"] = { name = "+Refactor" },
-    --         },
-    --     },
-    -- },
-    -- VIM APM Haven't found much use, will revist at a later date
-    -- {
-    --     "ThePrimeagen/vim-apm",
-    --     opts = {},
-    --     config = function(_, opts)
-    --         local apm = require("vim-apm")
-    --         apm:setup({})
-    --         vim.keymap.set("n", "<leader>apm", function()
-    --             apm:toggle_monitor()
-    --         end)
-    --     end,
-    -- },
+    {
+        "folke/which-key.nvim",
+        opts = {
+            preset = "modern",
+        },
+    },
 }
