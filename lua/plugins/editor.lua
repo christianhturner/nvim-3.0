a33b5a5e3a3d0d9391d7cd1ccf303ad0153c2e21
Christian Turner <cturnrq@amazon.com>
2025-10-22 12:52:55 -0400

    OBsidian integration

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,89 @@
+local user = os.getenv("USER")
+local obsidian_directory = "/Users/" .. user .. "/Documents/Obsidian"
+local OBSIDIAN_VAULT = vim.fn.resolve(obsidian_directory)
 return {
+
+    -- {
+    --     "MeanderingProgrammer/render-markdown.nvim",
+    --     opts = {
+    --         completions = {
+    --             lsp = {
+    --                 enabled = true,
+    --             },
+    --         },
+    --     },
+    -- },
+    -- {
+    --     "saghen/blink.cmp",
+    --     opts = {
+    --         sources = {
+    --             per_filetype = {
+    --                 markdown = { inherit_defaults = true },
+    --             },
+    --         },
+    --     },
+    -- },
+
+    {
+        "epwalsh/obsidian.nvim",
+        version = "*",
+        lazy = true,
+        ft = "markdown",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        events = { "BufReadPost" .. OBSIDIAN_VAULT .. "/**.md" },
+        ---@module 'obsidian'
+        ---@type obsidian.config
+        opts = {
+            ui = { enable = true },
+            frontmatter = { enabled = true },
+            workspaces = {
+                {
+                    name = "My Vault",
+                    path = OBSIDIAN_VAULT,
+                },
+            },
+            completion = {
+                nvim_cmp = true,
+                -- min_chars = 2,
+            },
+        },
+        config = function(_, opts)
+            require("obsidian").setup(opts)
+
+            local cmp = require("cmp")
+            cmp.register_source("obsidian", require("cmp_obsidian").new())
+            cmp.register_source("obsidian_new", require("cmp_obsidian_new").new())
+            cmp.register_source("obsidian_tags", require("cmp_obsidian_tags").new())
+        end,
+    },
+
+    {
+        "saghen/blink.cmp",
+        dependencies = { "saghen/blink.compat" },
+        opts = {
+            sources = {
+                default = { "obsidian", "obsidian_new", "obsidian_tags" },
+                providers = {
+                    obsidian = {
+                        name = "obsidian",
+                        module = "blink.compat.source",
+                    },
+                    obsidian_new = {
+                        name = "obsidian_new",
+                        module = "blink.compat.source",
+                    },
+                    obsidian_tags = {
+                        name = "obsidian_tags",
+                        module = "blink.compat.source",
+                    },
+                },
+            },
+        },
+    },
     {
         "mbbill/undotree",
         cmd = "UndotreeToggle",
